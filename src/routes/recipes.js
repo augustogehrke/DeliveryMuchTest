9e7c4aea42aefc83e649675ebd8ce7f4f5e7b9c6
Augusto Gehrke <augustogehrke@outlook.com>
2020-11-12 00:36:46 -0300

    Adição do express-validator para permitir fornecer no máximo 3 ingredientes

--- src/routes/recipes.js
+++ src/routes/recipes.js
@@ -1,9 +1,22 @@
 const express = require('express')
 const routes = express.Router()
 const recipesController = require('../controllers/recipes')
+const { check, validationResult } = require('express-validator')
 
-routes.get('/', async (req, res) => {
+routes.get('/', [
+  check('i').custom(i => {
+    const ingredients = i.split(',')
+    if (ingredients.length > 3) {
+      throw new Error('Permitido no máximo 3 ingredientes')
+    }
+  })
+], async (req, res) => {
   try {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+
     const data = await recipesController.getRecipes(req.query.i)
     res.json(data)
   } catch (error) {
